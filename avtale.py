83db0214ebc67ff575bf7b67c03d82d05461b263
milael <milaelm@gmail.com>
2022-11-01 16:44:50 +0100

    Slettet str metoden for å gjøre endringer

--- avtale.py
+++ avtale.py
@@ -1,11 +1,11 @@
 import datetime
 from datetime import datetime
 
+
 class Avtale:
     def __init__(self, tittel, sted, starttidspunkt, varighet):
         self.tittel = tittel
         self.sted = sted
         self.starttidspunkt = starttidspunkt
         self.varighet = varighet
-    def __str__(self):
-        print(f'Du har {self.tittel} på {self.sted} fra {self.starttidspunkt} til {self.starttidspunkt + self.varighet}')
+   
