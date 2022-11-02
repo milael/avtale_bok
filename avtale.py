2873de9430b2b29d4cd9a2b1c03b2d9c3c6f3f13
milael <milaelm@gmail.com>
2022-11-02 12:01:51 +0100

    Update avtale.py

--- avtale.py
+++ avtale.py
@@ -1,10 +1,7 @@
 import datetime
 from datetime import datetime, timedelta
-import csv
 
-
-''' Milael:
-    d) Lag en klasse for en avtale. En avtale skal minimum ha:
+''' d) Lag en klasse for en avtale. En avtale skal minimum ha:
         a. En tittel som sier hva denne avtalen er (streng)
         b. Et sted (streng)
         c. Et starttidspunkt (datetime objekt, se hint nederst)
@@ -17,8 +14,7 @@ class Appointment:
         self.start_time = start_time
         self.duration = duration
 
-    ''' Milael:
-        e) Lag en __str__ metode for avtaler som returnerer en streng 
+    ''' e) Lag en __str__ metode for avtaler som returnerer en streng 
         som kan skrives ut med en print-setning slik at du får skrevet ut avtalen 
         med alle egenskapene til avtalen på et leselig format for brukeren.'''
 
@@ -26,16 +22,12 @@ class Appointment:
         print(f'Avtalen: {self.titel} foregår kl. {self.start_time}'
               f'Sted: {self.place} Varighet: {self.duration}')
 
-
-
-''' Milael:
-    f) Lag en funksjon som lar brukeren skrive inn en ny avtale. 
+''' f) Lag en funksjon som lar brukeren skrive inn en ny avtale. 
     Funksjonen skal bruke input-funksjonen til å lese inn egenskapene til avtalen 
     og skal sjekke at det brukeren skriver er gyldig, for eksempel at varighet er et tall. 
     Funksjonen skal returnere et avtale-objekt'''
 
-''' Mehdi: 
-    g) Lag en funksjon som skriver ut ei liste med avtaler til skjermen. 
+''' g) Lag en funksjon som skriver ut ei liste med avtaler til skjermen. 
     Funksjonen skal minimum skrive ut indeksen til avtalen i lista og tittel til avtalen. 
     Den kan alternativt skrive ut indeksen til avtalen 
     og så hele avtalen som definert i __str__ metoden. 
@@ -43,26 +35,15 @@ class Appointment:
     som skal være en overskrift som funksjonen skriver ut før avtalene i lista. 
     Funksjonen skal inkludere indeksen til hver avtale i utskriften. '''
 
-appointment_list = []
-
-
-def print_appointment():
-    for i in appointment_list:
-        print(appointment_list[i])
-
-
-print_appointment()
-
-
-''' Mehdi:
-    h) Lag en funksjon som lagrer ei liste med avtaler til ei tekstfil. 
+''' h) Lag en funksjon som lagrer ei liste med avtaler til ei tekstfil. 
     Tenk over hva som vil være et fornuftig format for ei slik tekstfil.'''
 
 
 
 
-fields=['first','second','third']
 
-with open('appointment_file.csv', 'a') as f:
-    writer = csv.writer(f)
-    writer.writerow(fields)
+
+
+
+
+
