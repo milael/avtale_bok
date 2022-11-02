9baae9defdf1ca541e5a594fc65697f1cb7e4064
Mehdi Mahboobi <111884243+mahboobi1@users.noreply.github.com>
2022-11-02 10:56:34 +0100

    Update avtale.py

--- avtale.py
+++ avtale.py
@@ -1,6 +1,11 @@
 import datetime
 from datetime import datetime, timedelta
 
+''' d) Lag en klasse for en avtale. En avtale skal minimum ha:
+        a. En tittel som sier hva denne avtalen er (streng)
+        b. Et sted (streng)
+        c. Et starttidspunkt (datetime objekt, se hint nederst)
+        d. En varighet i minutter (int)'''
 
 class Avtale:
     def __init__(self, tittel, sted, starttidspunkt, varighet):
@@ -9,11 +14,15 @@ class Avtale:
         self.starttidspunkt = starttidspunkt
         self.varighet = varighet
 
+    ''' e) Lag en __str__ metode for avtaler som returnerer en streng 
+        som kan skrives ut med en print-setning slik at du får skrevet ut avtalen 
+        med alle egenskapene til avtalen på et leselig format for brukeren.'''
+
     def __str__(self):
         print(f'Avtalen: {self.tittel} foregår kl. {self.starttidspunkt}'
               f'Sted: {self.sted} Varighet: {self.varighet}')
 
-# det er en test
+
 
 
 
